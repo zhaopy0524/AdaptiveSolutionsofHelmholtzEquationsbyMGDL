f7968380ed67750e5d8ac804e1e0509e900b9d2b
Peiyao Zhao <zhaopy23@mails.jlu.edu.cn>
2025-05-13 13:14:49 +0800

    Update main.py

--- Adaptive FD-MGDL for Helmholtz/TwoDimensionalHelmholtzEquation/MGDL/main.py
+++ Adaptive FD-MGDL for Helmholtz/TwoDimensionalHelmholtzEquation/MGDL/main.py
@@ -12,18 +12,18 @@ mini_batch = False
 k = 50
 
 #set structure for MGDL
-mul_layers_dims =  [[2, 256, 256, 1],[256,256,1],[256,256,1],[256,256,1],[256,256,1],[256,256,1]]                  # this is the structure for MGDL
+mul_layers_dims =  [[2, 256, 256, 1], [256, 256, 1], [256, 256, 1], [256, 256, 1], [256, 256, 1], [256, 256, 1]]                  # this is the structure for MGDL
 #set activation for MGDL for each grade
-activation = ['sin','sin','relu','relu','relu','relu']
+activation = ['sin', 'sin', 'relu', 'relu', 'relu', 'relu']
 #set train epoch for each grade
-mul_epochs = [400,3000,3000,2500,2500,1000]
+mul_epochs = [400, 3000, 3000, 2500, 2500, 1000]
 
 stop_criterion = [1e-09,1e-09,1e-09,1e-09,1e-09,1e-09]
 
 data = generate_data(k)  
 
-max_learning_rate = [0.1,0.01,0.001,0.001,0.001,0.0001]
-min_learning_rate = [0.01,0.001,0.0001,0.001,0.001,0.001]
+max_learning_rate = [0.1, 0.01, 0.001, 0.001, 0.001, 0.001]
+min_learning_rate = [0.01, 0.001, 0.0001, 0.001, 0.001, 0.001]
 multi_grade_dnn(data, stop_criterion, max_learning_rate, min_learning_rate, mul_layers_dims, mul_epochs, SGD, mini_batch, activation, k)
 
 
