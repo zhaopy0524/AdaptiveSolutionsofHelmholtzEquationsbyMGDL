f9d2b58c054da6b55816b61540e6713b3ee498d2
Peiyao Zhao <zhaopy23@mails.jlu.edu.cn>
2025-05-13 14:56:45 +0800

    Update main.py

--- Adaptive FD-MGDL for Helmholtz/Concave Model/main.py
+++ Adaptive FD-MGDL for Helmholtz/Concave Model/main.py
@@ -4,35 +4,19 @@
 from multigrade_dnn_main import multi_grade_dnn
 from data_generate import generate_data
 
-
 SGD = False
 mini_batch = False
 
 
-#set structure for MGDL
-mul_layers_dims =  [[2, 256, 256, 1]]                  # this is the structure for MGDL
-#set activation for MGDL for each grade
-activation = ['sin']
-#set train epoch for each grade
-mul_epochs = [1000]
-
-stop_criterion = [1e-09]
-
 data = generate_data()  
-
+#set structure for MGDL
+mul_layers_dims =  [[2, 256, 256, 1], [256, 256, 1], [256, 256, 1]]                  # this is the structure for MGDL
+#set activation for MGDL for each grade
+activation = ['sin', 'sin', 'relu']
+#set train epoch for each grade
+mul_epochs = [1000, 2000, 4000]
+stop_criterion = [1e-09, 1e-09, 1e-09]
 #set max learning rate and min learning rate for each grade
-MAXLr = [[1e-1],[1e-2],[1e-3],[1e-4],[1e-5]]
-MINLr = [[1e-1],[1e-2],[1e-3],[1e-4],[1e-5]]
-for i in range(0,5):
-    for j in range(i,5):
-        max_learning_rate = MAXLr[i]
-        min_learning_rate = MINLr[j]
-        multi_grade_dnn(data, stop_criterion, max_learning_rate, min_learning_rate, mul_layers_dims, mul_epochs, SGD, mini_batch, activation)
-
-
-
-
-
-
-
-
+max_learning_rate = [0.1, 0.1, 0.01]
+min_learning_rate = [0.1, 0.01, 0.0001]
+multi_grade_dnn(data, stop_criterion, max_learning_rate, min_learning_rate, mul_layers_dims, mul_epochs, SGD, mini_batch, activation)
