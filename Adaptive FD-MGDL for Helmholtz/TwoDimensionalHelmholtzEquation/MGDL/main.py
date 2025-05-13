a52e1ec2c9092d13c3b8f57dd05d98ad36cc0f7f
Peiyao Zhao <zhaopy23@mails.jlu.edu.cn>
2025-05-13 13:22:51 +0800

    Update main.py

--- Adaptive FD-MGDL for Helmholtz/TwoDimensionalHelmholtzEquation/MGDL/main.py
+++ Adaptive FD-MGDL for Helmholtz/TwoDimensionalHelmholtzEquation/MGDL/main.py
@@ -9,21 +9,31 @@ from data_generate import generate_data
 SGD = False
 mini_batch = False
 
+#------------------------------------------k=50--------------------------------------------------------------------------------------------------------------------------
 k = 50
-
 #set structure for MGDL
 mul_layers_dims =  [[2, 256, 256, 1], [256, 256, 1], [256, 256, 1], [256, 256, 1], [256, 256, 1], [256, 256, 1]]                  # this is the structure for MGDL
 #set activation for MGDL for each grade
 activation = ['sin', 'sin', 'relu', 'relu', 'relu', 'relu']
 #set train epoch for each grade
 mul_epochs = [400, 3000, 3000, 2500, 2500, 1000]
-
-stop_criterion = [1e-09,1e-09,1e-09,1e-09,1e-09,1e-09]
-
+stop_criterion = [1e-09, 1e-09, 1e-09, 1e-09, 1e-09, 1e-09]
 data = generate_data(k)  
-
 max_learning_rate = [0.1, 0.01, 0.001, 0.001, 0.001, 0.001]
 min_learning_rate = [0.01, 0.001, 0.0001, 0.001, 0.001, 0.001]
 multi_grade_dnn(data, stop_criterion, max_learning_rate, min_learning_rate, mul_layers_dims, mul_epochs, SGD, mini_batch, activation, k)
 
 
+# #------------------------------------------k=100--------------------------------------------------------------------------------------------------------------------------
+# k = 100
+#set structure for MGDL
+# mul_layers_dims =  [[2, 256, 256, 1], [256, 256, 1], [256, 256, 1], [256, 256, 1]]                  # this is the structure for MGDL
+# #set activation for MGDL for each grade
+# activation = ['sin', 'sin', 'relu', 'relu']
+# #set train epoch for each grade
+# mul_epochs = [500, 2000, 2000, 2000]
+# stop_criterion = [1e-09, 1e-09, 1e-09, 1e-09]
+# data = generate_data(k)  
+# max_learning_rate = [0.1, 0.1, 0.01, 0.01]
+# min_learning_rate = [0.1, 0.01, 0.001, 0.001]
+# multi_grade_dnn(data, stop_criterion, max_learning_rate, min_learning_rate, mul_layers_dims, mul_epochs, SGD, mini_batch, activation, k)
