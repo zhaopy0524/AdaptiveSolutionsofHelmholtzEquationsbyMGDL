eb6ba49b91af6e7948ab6a1e2bccda8f6cb8f04e
Peiyao Zhao <zhaopy23@mails.jlu.edu.cn>
2025-05-13 14:04:12 +0800

    Update main.py

--- Adaptive FD-MGDL for Helmholtz/TwoDimensionalHelmholtzEquation/SGDL/main.py
+++ Adaptive FD-MGDL for Helmholtz/TwoDimensionalHelmholtzEquation/SGDL/main.py
@@ -4,22 +4,17 @@ from singlegrade_dnn_main import single_dnn_main
 
 
 #set parameter
-k = 50
-
-
 SGD = False
 mini_batch_size = False
 
 
+#---------------------------------------------------------------k=50---------------------------------------------------------------------------
+k = 50
 #set structure for SGDL
-layers_dims = [2,256,256,1]                                                  # this is the structure for SGDL
+layers_dims = [2, 256, 256, 256, 256, 256, 256, 256, 1]                                                  # this is the structure for SGDL
 #set train epoch
-epochs = 500
-
-
+epochs = 15000
 #set max learning rate and min learning rate 
-max_learning_rate = 1e-1                                            # the maximum learning rate, denote as t_max in the paper
-min_learning_rate = 1e-1                                            # the minimum learning rate, denote as t_min in the paper
-
-
+max_learning_rate = 1e-3                                            # the maximum learning rate, denote as t_max in the paper
+min_learning_rate = 1e-4                                            # the minimum learning rate, denote as t_min in the paper
 single_dnn_main( layers_dims, max_learning_rate, min_learning_rate, epochs, mini_batch_size, SGD, k )
